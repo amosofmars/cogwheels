6fe7727ce7efcdd883a58b645224fbea306fe015
Amos Mars <amosofmars@gmail.com>
2019-04-02 21:11:24 +0200

    init

---
+++ genpng.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+inkscape -z -e gearpoly0.png  gearpoly0.svg
+inkscape -z -e gearpoly1.png  gearpoly1.svg
+inkscape -z -e gearpoly2.png  gearpoly2.svg
+inkscape -z -e gearpoly3.png  gearpoly3.svg
